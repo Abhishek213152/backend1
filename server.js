5582bb670c9d2da4c0fa5c67c16b8470bd68f5c6
Abhishek Jha <131297713+Abhishek213152@users.noreply.github.com>
2024-04-14 12:19:12 +0530

    server.js

--- server.js
+++ server.js
@@ -9,25 +9,31 @@ import postRoutes from "./routes/posts.js"
 const app = express();
 dotenv.config();
 
+// Middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+// Routes
 app.use("/posts", postRoutes);
-
-app.get("/", (req, res)=>{
-  res.send("Hello to travel API")
-})
+app.get("/", (req, res) => {
+  res.send("Hello to travel API");
+});
 
 const PORT = process.env.PORT || 5000;
 
-mongoose
-  .connect(process.env.CONNECTION_URL, {
-    dbName: "travelapp", 
-  })
-  .then(() =>
+// Connect to MongoDB
+const uri = process.env.MONGO_CONNECTION_STRING;
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MongoDB connected successfully");
+
+    // Start the server
     app.listen(PORT, () => {
       console.log(`Server running on port: ${PORT}`);
-    })
-  )
-  .catch((error) => console.log(error.message));
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error.message);
+  });
